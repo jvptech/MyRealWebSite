649dbc45998b68457aaf82d20b296a52b60c78cc
Vinícius Miranda <jvptech@outlook.com>
2025-02-11 19:05:26 -0500

    Modal dialog (#6)
    * Modal dialog
    
    * Firt unit test block
    
    * Main layout
    
    * Pipeline unit test

---
+++ BlazorApp.Tests/Components/Layout/MainLayoutComponentTests.cs
@@ -0,0 +1,71 @@
+﻿using BlazorApp.Components.Layout;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using Moq;
+using System.Globalization;
+
+namespace BlazorAppTests.Components.Layout;
+
+public class MainLayoutComponentTests : TestContext
+{
+    private readonly Mock<IJSRuntime> _jsRuntimeMock = new();
+    private readonly IRenderedComponent<MainLayout> cut;
+
+    public MainLayoutComponentTests()
+    {
+        Services.AddSingleton<IJSRuntime>(_jsRuntimeMock.Object);
+         cut = RenderComponent<MainLayout>();
+    }
+
+    [Fact]
+    public void ClickingButton_ShouldShowModalDialog()
+    {
+        // Arrange & Act
+        var button = cut.Find("button");
+        button.Click();
+
+        // Assert
+        Assert.Contains("myModal", cut.Markup);
+    }
+
+    [Fact]
+    public void DefaultState_ShouldNotShowModalDialog()
+    {
+        // Arrange & Act & Assert
+        Assert.DoesNotContain("myModal", cut.Markup);
+    }
+
+    [Fact]
+    public void Button_ShouldDisplayCurrentCulture()
+    {
+        // Arrange & Act
+        var button = cut.Find("button");
+
+        // Assert
+        Assert.Contains(CultureInfo.CurrentCulture.DisplayName, button.TextContent);
+    }
+
+    [Fact]
+    public void OnOpenDialog_ShouldSetShowModalDialogToTrue()
+    {
+        // Arrange & Act
+        cut.InvokeAsync(() => cut.Instance.OnOpenDialog());
+
+        // Assert
+        Assert.True(cut.Instance.ShowModalDialog);
+    }
+
+    [Fact]
+    public void OnCloseDialog_ShouldSetShowModalDialogToFalse()
+    {
+        // Arrange
+        cut.Instance.ShowModalDialog = true;
+
+        // Act
+        cut.InvokeAsync(() => cut.Instance.OnCloseDialog());
+
+        // Assert
+        Assert.False(cut.Instance.ShowModalDialog);
+    }
+}
---
+++ BlazorApp.Tests/Components/Pages/HomeComponentTests.cs
@@ -0,0 +1,54 @@
+﻿using BlazorApp.Components.Pages;
+using Bunit;
+using System.Globalization;
+
+namespace BlazorAppTests.Components.Pages;
+
+public class HomeComponentTests : TestContext
+{
+    private readonly IRenderedComponent<Home> cut;
+
+    public HomeComponentTests()
+    {
+        cut = RenderComponent<Home>();
+    }
+
+    [Fact]
+    public void HomePage_ShouldRender_CorrectH1()
+    {
+        // Arrange & Act
+           var heading = cut.Find("h1");
+
+        // Assert
+        heading.MarkupMatches("<h1>Hello, world!</h1>");
+    }
+
+    [Fact]
+    public void HomePage_ShouldRender_CorrectH3()
+    {
+        // Arrange & Act
+        var heading = cut.Find("h3");
+
+        // Assert
+        heading.MarkupMatches("<h3>Culture Client</h3>");
+    }
+
+    [Fact]
+    public void HomePage_ShouldRender_CorrectCultureInfo()
+    {
+        // Arrange
+        var expectedCulture = CultureInfo.CurrentCulture.ToString();
+        var expectedUICulture = CultureInfo.CurrentUICulture.ToString();
+
+        // Act
+        var heading = cut.Find("ul");
+
+        // Assert
+        heading.MarkupMatches($@"
+            <ul>
+                <li><b>CurrentCulture</b>: {expectedCulture}</li>
+                <li><b>CurrentUICulture</b>: {expectedUICulture}</li>
+            </ul>
+        ");
+    }
+}
